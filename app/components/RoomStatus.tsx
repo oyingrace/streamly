391d9c25dff65b5a0b2894a736b2ca43f9a1e3f2
oyingrace <oyingrace307@gmail.com>
2025-08-15 16:02:45 +0100

    update

--- app/components/RoomStatus.tsx
+++ app/components/RoomStatus.tsx
@@ -24,7 +24,7 @@ export default function RoomStatus({
   // Host start button
   if (!isStreaming && isHost) {
     return (
-      <div className="absolute bottom-24 sm:bottom-32 left-0 right-0 z-10 flex justify-center pb-8 sm:pb-12">
+      <div className="absolute bottom-24 sm:bottom-48 left-0 right-0 z-10 flex justify-center pb-8 sm:pb-12">
         <button 
           onClick={onStartStream}
           disabled={isInitializing || !zegoEngine || !isLoggedIn}
