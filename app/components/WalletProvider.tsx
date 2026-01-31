39493aa848e03aac515c442d3de5f399af4c2ce8
oyingrace <oyingrace307@gmail.com>
2026-01-31 01:03:42 +0100

    import solana adapter

--- app/components/WalletProvider.tsx
+++ app/components/WalletProvider.tsx
@@ -2,3 +2,8 @@
 
 import { FC, ReactNode, useMemo } from "react";
 import dynamic from "next/dynamic";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
